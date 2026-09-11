2016024928e61a6827f6ea2ab020f3012403201a
Gerson Borja <hipersocial08@gmail.com>
2026-09-10 23:07:42 -0600

    Estructura base se comenzara por el frontend

---
+++ components/Menu.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+const Menu = () => {
+    return (
+        <header className='px-4 py-6 bg-white flex items-center justify-between border-b-[#c9c9c9] border-b'>
+            <h1 className='font-bold'>
+                ROTULA<span className='text-orange-500'>.APP</span>
+            </h1>
+            <div>
+                <Link href="/login" className='bg-black px-4 py-2 text-white rounded-xl mr-4'>Iniciar sesion</Link>
+                <Link href="/registro" className='bg-black px-4 py-2 text-white rounded-xl'>Registrate</Link>
+            </div>
+        </header>
+    )
+}
+
+export default Menu;
